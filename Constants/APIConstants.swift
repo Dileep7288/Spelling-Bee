7c91ccdaaff3da5dfc339f0ce67908a4df730828
Dileep724 <dileep.s@station-s.org>
2025-03-22 12:38:22 +0530

    Stop tracking Constants folder

--- Constants/APIConstants.swift
+++
@@ -1,6 +0,0 @@
-struct APIConstants {
-    static let baseURL = "http://172.16.21.7:8002"
-    static let spellBeeEndpoint = "\(baseURL)/api/spellbee"
-    static let submitGame = "\(baseURL)/api/submit/"
-    static let practiceApi = "https://random-word-api.vercel.app/api?words=5"
-}
